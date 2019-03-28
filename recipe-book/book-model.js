df3ce01d38aac9cabc592adc940373c723396083
ills5771 <ilyayelizarov1@gmail.com>
2019-03-28 12:33:32 -0700

    added book-model.js

---
+++ recipe-book/book-model.js
@@ -0,0 +1,16 @@
+const db = require("../data/dbConfig.js");
+
+module.exports = {
+  find,
+  findById
+};
+
+function find() {
+  return db("roles");
+}
+
+function findById(id) {
+  return db("roles")
+    .where({ id })
+    .first();
+}
